f3b4e31e32dc7afcc7539024f657cc3ef268010f
Amr Wagdy <wagdy@scaleflex.com>
2024-10-20 03:23:13 +0300

    chore: Setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,9 @@
-import type { Config } from "tailwindcss";
+const { nextui } = require('@nextui-org/react')
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+  theme: {},
+  plugins: [nextui()],
+}
