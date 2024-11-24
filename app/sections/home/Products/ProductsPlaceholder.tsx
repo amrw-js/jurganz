b62d56350cd83a22cd15c5574fe6460f12240d4e
Amr Wagdy <wagdy@scaleflex.com>
2024-11-24 18:18:32 +0200

    design: Fix spacing in the home page

--- app/sections/home/Products/ProductsPlaceholder.tsx
+++ app/sections/home/Products/ProductsPlaceholder.tsx
@@ -8,8 +8,8 @@ export const ProductsPlaceholder = () => {
   const { t } = useTranslation()
   return (
     <div className='flex flex-col items-center justify-center gap-5 lg:gap-10'>
-      <ProductsPlaceholderIcon className='size-36 text-white' />
-      <p className='text-lg font-semibold leading-9 lg:text-3xl'>{t('No Production lines added yet')}</p>
+      <ProductsPlaceholderIcon className='size-36 text-gray-300' />
+      <p className='text-lg font-semibold leading-9 text-gray-300 lg:text-3xl'>{t('No Production lines added yet')}</p>
       <Button color='primary'>
         <Link href='#'>{t('add_new_line')}</Link>
       </Button>
